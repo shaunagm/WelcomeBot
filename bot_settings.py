beacd90b3fe1cf9caac49c09b8e1b5e98397a5ef
Shauna <shaunagm@gmail.com>
2015-03-15 10:23:32 -0400

    Clean up typo, unnecessary import

--- bot_settings.py
+++ bot_settings.py
@@ -14,4 +14,4 @@ nick_source = "nicks.csv"
 # Bot text
 hello_list = ["hello", "hi", "hey", "yo", "sup"]
 help_list = ["help", "info", "faq", "explain_yourself"]
-welcome_message = "Welcome! THe channel is pretty quiet right now, so I thought I'd say hello, and ping some people that you're here. If no one responds for a whle, try emailing us at hello@openhatch.org or just try coming back later. FYI, you're now on my list of known nicknames, so I won't bother you again."
+welcome_message = "Welcome! The channel is pretty quiet right now, so I thought I'd say hello, and ping some people that you're here. If no one responds for a whle, try emailing us at hello@openhatch.org or just try coming back later. FYI, you're now on my list of known nicknames, so I won't bother you again."
